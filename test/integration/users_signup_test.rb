d32822fc6fd790ebcab031273f30d10ff8c1b00d
Sundar Vasan <vr.sundar@gmail.com>
2018-02-23 15:35:07 -0800

    Add form test. Update post url to /signup

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test 'invalid signup should fail' do
+    get signup_path
+    assert_select 'form[action="/signup"]'
+    assert_no_difference 'User.count' do
+      post signup_path, params: { user: { name: 'Some Name',
+                                          email: 'invalid@email',
+                                          password: 'abcdef',
+                                          password_confirmation: 'abcdef'}}
+    end
+    assert_select 'div#error_explanation'
+    assert_select 'div.alert', "The form contains 2 errors"
+    assert_select 'div.field_with_errors input', 2
+    assert_template 'users/new'
+  end
 end
