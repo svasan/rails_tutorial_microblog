bb535317c60c9c742bbe2db994f5874a11e69cc7
Sundar Vasan <vr.sundar@gmail.com>
2018-03-09 18:49:52 -0800

    9.1.1 Remember token and digest
    The schema has the digest via the previous generated migration.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,20 @@ class User < ApplicationRecord
             :format => {:with => VALID_EMAIL_REGEX}, :uniqueness => {:case_sensitive => false}
   has_secure_password
   validates :password, :presence => true, :length => {:minimum => 8}
+  attr_accessor :remember_token
 
   def User.digest(s)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
     BCrypt::Password.create(s, cost: cost)
   end
+
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
 end
