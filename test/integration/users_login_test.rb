6a6390ec98ad099c496e97c7e09bde7c3450c1b2
Sundar Vasan <vr.sundar@gmail.com>
2018-03-10 17:23:47 -0800

    Logout in 2 windows is already handled.
    since SessionHelper::log_out already checks logged_in?
    
    Update test.

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -19,19 +19,24 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     get login_path
     assert_select 'form[action="/login"]'
     post login_path, params: { session: {email: @user.email, password: "password"}}
-    #    assert_template 'users/show'
+    assert is_logged_in?
+
     assert_redirected_to @user
     follow_redirect!
     assert_template 'users/show'
     assert_select "a[href=?]", logout_path
     assert_select "a[href=?]", user_path(@user)
-    # No login
     assert_select "a[href=?]", login_path, count: 0
-    assert is_logged_in?
+
     delete logout_path
     assert_not is_logged_in?
     assert_redirected_to root_path
+
+    #Simulate a user clicking logout in a second window
+    delete logout_path
     follow_redirect!
     assert_select "a[href=?]", login_path, count: 1
+    assert_select "a[href=?]", logout_path, count: 0
+    assert_select "a[href=?]", user_path(@user), count: 0
   end
 end
