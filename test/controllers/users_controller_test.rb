acf827555f94943dceb193ca10288b524c66ffee
Sundar Vasan <vr.sundar@gmail.com>
2018-03-14 19:23:12 -0700

    10.4.1 Add admin attribute
    Also add test to make sure it cannot be set via http

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,6 +1,12 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user_foo = users(:foobar)
+    @user_joe = users(:joe)
+  end
+
   test "should get new" do
     get signup_path
     assert_response :success
@@ -9,5 +15,18 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   test "index should redirect when not logged in" do
     get users_path
     assert_redirected_to login_url
+  end
+
+  test "admin attribute cannot be set" do
+    log_in_as(@user_joe)
+    assert_not @user_joe.admin?
+    patch user_path(@user_joe), params: {
+      user: {
+        password: "otherpass",
+        password_confirmation: "otherpass",
+        admin: true
+      }
+    }
+    assert_not @user_joe.reload.admin?
   end
 end
