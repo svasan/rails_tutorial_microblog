0a1e66e5ba7e24fe637d206fb28ff1e53e526420
Sundar Vasan <vr.sundar@gmail.com>
2018-03-14 19:54:09 -0700

    10.4.3 Add tests for User#destroy

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -3,16 +3,29 @@ require 'test_helper'
 class UsersIndexTest < ActionDispatch::IntegrationTest
 
   def setup
-    @user_foo= users(:foobar)
+    @admin = users(:foobar)
+    @user_joe = users(:joe)
   end
 
   test "index including pagination" do
-    log_in_as(@user_foo)
+    log_in_as(@admin)
     get users_path
     assert_template "users/index"
     assert_select "div.pagination", count: 2
     User.paginate(page: 1).each do |user|
-      assert_select "a[href=?]", user_path(user), text:user.name
+      assert_select "a[href=?]", user_path(user), text: user.name
+      unless @admin == user
+        assert_select "a[href=?]", user_path(user), text: "delete", count: 1
+      end
     end
+    assert_difference "User.count", -1 do
+      delete user_path(@user_joe)
+    end
+  end
+
+  test "index as non-admin" do
+    log_in_as(@user_joe)
+    get users_path
+    assert_select "a", text: "delete", count: 0
   end
 end
