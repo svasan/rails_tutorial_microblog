7b04b06769e88682caa02ec78f016069fd6dd150
Sundar Vasan <vr.sundar@gmail.com>
2018-03-16 10:58:52 -0700

    13.1.2 Add minor validations for microposts

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -1,7 +1,28 @@
 require 'test_helper'
 
 class MicropostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user_foo = users(:foobar)
+    @micropost = Micropost.new(content: "Random Text", user_id: @user_foo.id)
+  end
+
+  test "should be valid" do
+    assert @micropost.valid?
+  end
+
+  test "microposts should always have a user_id" do
+    @micropost.user_id = nil
+    assert_not @micropost.valid?
+  end
+
+  test "content should not be empty" do
+    @micropost.content = "    "
+    assert_not @micropost.valid?
+  end
+
+  test "content should not be more than 140 chars" do
+    @micropost.content = "A" * 141
+    assert_not @micropost.valid?
+  end
 end
