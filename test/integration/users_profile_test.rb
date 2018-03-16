d6b63592ef115bb2ee89bf4fdca09c7cd5d6d3e2
Sundar Vasan <vr.sundar@gmail.com>
2018-03-16 15:34:27 -0700

    13.2.3 Add test for the profile page with microposts

--- test/integration/users_profile_test.rb
+++ test/integration/users_profile_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class UsersProfileTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  # include ApplicationHelper
+
+  def setup
+    @user_foo = users(:foobar)
+  end
+
+  test "user profile display" do
+    get user_path(@user_foo)
+    assert_template 'users/show'
+    assert_select 'title', full_title(@user_foo.name)
+
+    assert_select 'h1', text: @user_foo.name
+    assert_select 'h1>img.gravatar'
+    assert_match "Microposts (#{@user_foo.microposts.count})", response.body
+    assert_select "div.pagination", count: 1
+    @user_foo.microposts.paginate(page: 1).each do |post|
+      # html escape content as that is how the response body
+      # is. Otherwise 's esp won't match.
+      assert_match ERB::Util.html_escape(post.content), response.body
+    end
+  end
 end
