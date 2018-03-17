ed64dfe81be8adc8e6ff62508f6c21d38cb0e44f
Sundar Vasan <vr.sundar@gmail.com>
2018-03-17 12:02:57 -0700

    Test that Micropost#destroy redirects to root for incorrect user.
    Also add micropost fixtures by other users.

--- test/controllers/microposts_controller_test.rb
+++ test/controllers/microposts_controller_test.rb
@@ -19,4 +19,13 @@ class MicropostsControllerTest < ActionDispatch::IntegrationTest
     end
     assert_redirected_to login_url
   end
+
+  test "destroy should redirect for incorrect user" do
+    log_in_as(users(:lana))
+    assert_no_difference 'Micropost.count' do
+      delete micropost_path(@orange_post)
+    end
+    assert_redirected_to root_url
+  end
+
 end
