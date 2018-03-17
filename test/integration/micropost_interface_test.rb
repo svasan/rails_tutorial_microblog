7f55d0f07bdbb2e11133b978194f944f0b67e1b3
Sundar Vasan <vr.sundar@gmail.com>
2018-03-17 13:23:00 -0700

    13.3.5 Add integration test for the micropost interface.
    Mainly test the home page but also test that deletes don't exist in
    the user profile of another user.
    
    Fix micropost fixtures that refer to non-existent user.

--- test/integration/micropost_interface_test.rb
+++ test/integration/micropost_interface_test.rb
@@ -1,7 +1,58 @@
 require 'test_helper'
 
 class MicropostInterfaceTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user_foo = users(:foobar)
+  end
+
+  test "micropost interface on the home page" do
+    log_in_as(@user_foo)
+    get root_url
+    assert_match @user_foo.name, response.body
+    assert_select "form.new_micropost", count: 1
+    assert_match "#{@user_foo.microposts.count} microposts", response.body
+
+    # Invalid submission
+    assert_no_difference 'Micropost.count' do
+      post microposts_path, params: { micropost: { content: "" }}
+    end
+
+    # Valid submission
+    text = "Some Unique Text. Maybe.".reverse
+    assert_difference 'Micropost.count', 1 do
+      post microposts_path, params: { micropost: { content: text }}
+    end
+    assert_redirected_to root_url
+    follow_redirect!
+    assert_match text, response.body
+
+    # Microposts and delete links exist
+    microposts = @user_foo.microposts.paginate(page: 1)
+    microposts.each do |m|
+      assert_match ERB::Util.html_escape(m.content), response.body
+      assert_select "a[href=?]", micropost_path(m), text: "delete"
+    end
+
+    # Delete works.
+    assert_difference 'Micropost.count', -1 do
+      delete micropost_path(microposts.last)
+    end
+
+    # No deletes in some other user's profile page
+    get user_path(users(:joe))
+    assert_select "a", text: "delete", count: 0
+
+    # User with no microposts
+    log_in_as(users(:malory))
+    get root_path
+    assert_match "0 microposts", response.body
+    assert_difference 'Micropost.count', 1 do
+      post microposts_path, params: { micropost: { content: "This is Malory!" }}
+    end
+    assert_redirected_to root_url
+    follow_redirect!
+    assert_match "1 micropost", response.body
+  end
+
 end
