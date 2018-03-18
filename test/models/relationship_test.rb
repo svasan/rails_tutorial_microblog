1859603f03711fe21310efd489dcd9066d7636c2
Sundar Vasan <vr.sundar@gmail.com>
2018-03-17 20:21:58 -0700

    14.1.3 Define some of the associations between User and Relationship
    User has many :following assoc, a Relationship
    
    Relationship belongs to follower and followed, both Users.
    
    Add basic test and remove the generate fixtures.

--- test/models/relationship_test.rb
+++ test/models/relationship_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class RelationshipTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @relationship = Relationship.new(follower_id: users(:joe).id,
+                                     followed_id: users(:lana).id)
+  end
+
+  test "should be valid" do
+    assert @relationship.valid?
+  end
+
+  test "nil follower should not be valid" do
+    @relationship.follower_id = nil
+    assert_not @relationship.valid?
+  end
+
+  test "nil followed should not be valid" do
+    @relationship.followed_id = nil
+    assert_not @relationship.valid?
+  end
 end
