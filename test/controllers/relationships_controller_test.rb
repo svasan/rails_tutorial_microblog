fb39c341177b73a9a582ab9da4d78bc560c46956
Sundar Vasan <vr.sundar@gmail.com>
2018-03-18 18:37:07 -0700

    14.2.4 Implement standard follow and unfollow.
    Add tests to verify that these need a logged in user.

--- test/controllers/relationships_controller_test.rb
+++ test/controllers/relationships_controller_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class RelationshipsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "post redirects when not logged in" do
+    assert_no_difference "Relationship.count" do
+      post relationships_path, params: { followed_id: users(:foobar).id }
+    end
+    assert_redirected_to login_url
+  end
+
+  test "delete redirects when not logged in" do
+    assert_no_difference "Relationship.count" do
+      delete relationship_path(relationships(:one))
+    end
+    assert_redirected_to login_url
+  end
 end
